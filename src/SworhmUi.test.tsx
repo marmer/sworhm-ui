9b7f0e01e6b27a716ed608a029b96bc76f7778ea
MarMer <1055445+marmer@users.noreply.github.com>
2019-05-18 17:13:12 +0200

    App renamed to SworhmUi

--- src/SworhmUi.test.tsx
+++ src/SworhmUi.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import SworhmUi from './SworhmUi';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<SworhmUi/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });

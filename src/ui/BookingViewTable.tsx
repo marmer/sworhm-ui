79a8a08f70bfb6bc16009e79dfdd49037fae215e
marmer <Mariano.Mertinat@gmail.com>
2019-05-19 15:24:45 +0200

    a little connection between a "remote" entry and the related object

--- src/ui/BookingViewTable.tsx
+++ src/ui/BookingViewTable.tsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import BookingEntryView from "./BookingEntryView";
 
 interface BookingTableViewState {
+    bookingEntries: string[]
 
 }
 
@@ -10,10 +11,33 @@ export interface BookingViewTableProps {
 }
 
 export default class BookingViewTable extends Component<BookingViewTableProps, BookingTableViewState> {
+
+    constructor(props: Readonly<BookingViewTableProps>) {
+        super(props);
+        this.state = {
+            bookingEntries: [
+                "http://some.server/booking/42/entry/21", "http://some.server/booking/42/entry/42",
+            ]
+        }
+    }
+
     render(): React.ReactElement {
         return <div>
-            <BookingEntryView/>
+            <EntryList bookingEntries={this.state.bookingEntries}/>
         </div>;
     }
 
+}
+
+interface EntryListProps {
+    bookingEntries: string[]
+
+}
+
+function EntryList(bookingEntries: EntryListProps) {
+    let entries = bookingEntries.bookingEntries.map(entryId => <BookingEntryView key={String(entryId)}
+                                                                                 location={entryId}/>);
+    return <>
+        {entries}
+    </>;
 }

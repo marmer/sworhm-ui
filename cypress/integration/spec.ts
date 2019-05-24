85daf216344ae92d542769cd191addc3af7eb8ad
marmer <mariano.mertinat@gmail.com>
2019-05-25 01:22:43 +0200

    a little cleanup

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -12,7 +12,7 @@ describe('Some Acceptance test', () => {
     cy.visit('http://localhost:3000');
     cy.get('.BookingEntryView')
         .its('length')
-        .should('eq', 2);
+        .should('eq', 1);
 
     cy.get('.BookingEntryView:last')
         .find('button[title=add]')
@@ -20,7 +20,7 @@ describe('Some Acceptance test', () => {
 
     cy.get('.BookingEntryView')
         .its('length')
-        .should('eq', 3);
+        .should('eq', 2);
   });
 
     it('should load with entries from the backend', function () {
@@ -31,16 +31,24 @@ describe('Some Acceptance test', () => {
       cy.visit('http://localhost:3000');
       cy.get('.BookingEntryView').eq(0)
           .find('.start').should('have.value', '00:55')
+      cy.get('.BookingEntryView').eq(0)
           .find('.duration').should('have.value', '01:13:00')
+      cy.get('.BookingEntryView').eq(0)
           .find('.description').should('have.value', 'another one bites the dust')
+      cy.get('.BookingEntryView').eq(0)
           .find('.ticket').should('have.value', 'JIRA-666')
+      cy.get('.BookingEntryView').eq(0)
           .find('.notes').should('have.value', 'knocking on heavens door');
 
       cy.get('.BookingEntryView').eq(1)
           .find('.start').should('have.value', '01:55')
-          .find('.duration').should('have.value', '01:13:00')
+      cy.get('.BookingEntryView').eq(1)
+          .find('.duration').should('have.value', '08:15:00')
+      cy.get('.BookingEntryView').eq(1)
           .find('.description').should('have.value', 'stay alive')
+      cy.get('.BookingEntryView').eq(1)
           .find('.ticket').should('have.value', 'JIRA-999')
+      cy.get('.BookingEntryView').eq(1)
           .find('.notes').should('have.value', 'cheek to cheek');
     });
 });

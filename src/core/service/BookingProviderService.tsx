3d3b7fdc7517b65f6f7a49b763283d6a8eb1bb2a
marmer <mariano.mertinat@gmail.com>
2019-05-26 11:47:52 +0200

    a little movement

--- src/core/service/BookingProviderService.tsx
+++ src/core/service/BookingProviderService.tsx
@@ -4,10 +4,3 @@ export default interface BookingProviderService {
     getBookingEntriesByDate(day: string): Promise<BookingEntry>;
 }
 
-class BookingProviderServiceImpl implements BookingProviderService {
-    getBookingEntriesByDate(day: string): Promise<BookingEntry> {
-        return new Promise<BookingEntry>((resolve, reject) => {
-            // TODO: marmer 25.05.2019 go on here!
-        });
-    }
-}

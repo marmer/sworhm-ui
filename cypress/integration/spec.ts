0ba161c4eb97b2853f16876dd98e0100a9983b13
MarMer <1055445+marmer@users.noreply.github.com>
2019-05-30 09:59:57 +0200

    switched some acceptance tests to use cypress testing library

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,59 +1,58 @@
+import "cypress-testing-library/add-commands"
+
 describe('Some Acceptance test', () => {
 
-  it('should load', function () {
-    cy.visit('http://localhost:3000');
-    cy.get('.SwormUi')
-        .find('h1')
-        .should('be.visible')
-        .and('have.text', 'Sworhm UI');
-  });
+    it('should load', function () {
+        cy.visit('http://localhost:3000');
+        cy.getByText('Sworhm UI')
+            .should('be.visible');
+    });
 
-  it('should be possible to add more entries', function () {
-    cy.visit('http://localhost:3000');
-    cy.get('.BookingEntryView')
-        .its('length')
-        .should('eq', 1);
+    it('should be possible to add more entries', function () {
+        cy.visit('http://localhost:3000');
+        cy.get('.BookingEntryView')
+            .its('length')
+            .should('eq', 1);
 
-    cy.get('.BookingEntryView:last')
-        .find('button[title=add]')
-        .click();
+        cy
+            .getAllByTitle('add').last()
+            .click();
 
-    cy.get('.BookingEntryView')
-        .its('length')
-        .should('eq', 2);
-  });
+        cy.get('.BookingEntryView')
+            .its('length')
+            .should('eq', 2);
+    });
 
     it('should load with entries from the backend', function () {
-    cy.server({ force404: true });
+        cy.server({force404: true});
 
-      cy.route("GET", "http://backend.de/api/booking-days/2002-02-01", "fixture:day_2002-02-01_entries.json")
-          .as("entriesLoad");
+        cy.route("GET", "http://backend.de/api/booking-days/2002-02-01", "fixture:day_2002-02-01_entries.json")
+            .as("entriesLoad");
 
-      cy.visit('http://localhost:3000').debug({
-        log: true
-      });
-      cy.wait("@entriesLoad");
-      cy.get('.BookingEntryView').eq(0)
-          .find('.start').should('have.value', '00:55');
-      cy.get('.BookingEntryView').eq(0)
-          .find('.duration').should('have.value', '01:13:00');
-      cy.get('.BookingEntryView').eq(0)
-          .find('.description').should('have.value', 'another one bites the dust');
-      cy.get('.BookingEntryView').eq(0)
-          .find('.ticket').should('have.value', 'JIRA-666');
-      cy.get('.BookingEntryView').eq(0)
-          .find('.notes').should('have.value', 'knocking on heavens door');
+        cy.visit('http://localhost:3000').debug({
+            log: true
+        });
+        cy.wait("@entriesLoad");
+        cy.getAllByPlaceholderText('09:25').eq(0)
+            .should('have.value', '00:55');
+        cy.getAllByPlaceholderText('2:17').eq(0)
+            .should('have.value', '01:13:00');
+        cy.getAllByPlaceholderText('What has been done').eq(0)
+            .should('have.value', 'another one bites the dust');
+        cy.getAllByPlaceholderText('TICKET-123').eq(0)
+            .should('have.value', 'JIRA-666');
+        cy.getAllByPlaceholderText('personal notes').eq(0)
+            .should('have.value', 'knocking on heavens door');
 
-
-      cy.get('.BookingEntryView').eq(1)
-          .find('.start').should('have.value', '01:55');
-      cy.get('.BookingEntryView').eq(1)
-          .find('.duration').should('have.value', '08:15:00');
-      cy.get('.BookingEntryView').eq(1)
-          .find('.description').should('have.value', 'stay alive');
-      cy.get('.BookingEntryView').eq(1)
-          .find('.ticket').should('have.value', 'JIRA-999');
-      cy.get('.BookingEntryView').eq(1)
-          .find('.notes').should('have.value', 'cheek to cheek');
+        cy.getAllByPlaceholderText('09:25').eq(1)
+            .should('have.value', '01:55');
+        cy.getAllByPlaceholderText('2:17').eq(1)
+            .should('have.value', '08:15:00');
+        cy.getAllByPlaceholderText('What has been done').eq(1)
+            .should('have.value', 'stay alive');
+        cy.getAllByPlaceholderText('TICKET-123').eq(1)
+            .should('have.value', 'JIRA-999');
+        cy.getAllByPlaceholderText('personal notes').eq(1)
+            .should('have.value', 'cheek to cheek');
     });
 });

8e0e4e0fee1b4a29b4e364d18c0026e3d1573b54
marmer <mariano.mertinat@gmail.com>
2019-05-30 09:25:43 +0200

    compile error fixed typescript testing library included correctly

--- src/ui/BookingDayView.tsx
+++ src/ui/BookingDayView.tsx
@@ -11,17 +11,10 @@ export interface BookingDayViewProps {
 }
 
 export default class BookingDayView extends Component<BookingDayViewProps, BookingDayViewState> {
-
-    constructor(props: Readonly<BookingDayViewProps>) {
-        super(props);
-    }
-
     render(): React.ReactElement {
         return <div>
             <BookingTableView
-                bookingProviderService={new RestBookingEntryService("http://backend.de/api/booking-days/2002-02-01")}
-                resourceReference="http://backend.de/api/booking-days/2002-02-01"
-                bookingDay="2002-02-01"/>
+                bookingProviderService={new RestBookingEntryService("http://backend.de/api/booking-days/2002-02-01")}/>
         </div>;
     }
 

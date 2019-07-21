0bfc580f1943e7f128462dec9fa7fa8c823de0be
marmer <mariano.mertinat@gmail.com>
2019-07-21 23:33:03 +0200

    - simple view to change the date added

--- src/SworhmUi.tsx
+++ src/SworhmUi.tsx
@@ -5,20 +5,54 @@ import RestCoreServiceFactory from "./RestCoreServiceFactory";
 import moment from "moment";
 
 
-export default class SworhmUi extends Component {
+interface SworhmUiState {
+    day: Date
+}
+
+export interface SworhmUiProps {
+
+}
+
+export default class SworhmUi extends Component<SworhmUiProps, SworhmUiState> {
+    private coreServiceFactory = new RestCoreServiceFactory();
+
+    constructor(props: SworhmUiProps, context: any) {
+        super(props, context);
+        this.state = {
+            day: new Date()
+        }
+    }
+
     render() {
-        const now = new Date();
         return (
             <div className="SwormUi">
                 <h1>Sworhm UI</h1>
+                <DayChooser initialDate={this.state.day} onChange={this.changeDate}/>
                 <BookingDayView
-                    day={this.toIsoDate(now)}
-                    coreServicesFactory={new RestCoreServiceFactory()}/>
+                    day={toIsoDate(this.state.day)}
+                    coreServicesFactory={this.coreServiceFactory}/>
             </div>
         );
     }
 
-    private toIsoDate = (now: Date) => {
-        return moment(now).format('YYYY-MM-DD');
-    }
+    private changeDate = (newDate: Date) => this.setState({day: newDate});
+
+}
+const toIsoDate = (now: Date) => {
+    return moment(now).format('YYYY-MM-DD');
+};
+
+function DayChooser(props: {
+    initialDate: Date
+    onChange: (newDate: Date) => void
+}) {
+    const goToNextDay = () => props.onChange(moment(props.initialDate).add(1, "day").toDate());
+    const goToPreveousDay = () => props.onChange(moment(props.initialDate).add(-1, "day").toDate());
+    return <div className="container">
+        <div className="row">
+            <button className="col" onClick={goToPreveousDay}>-</button>
+            <div className="col">{toIsoDate(props.initialDate)}</div>
+            <button className="col" onClick={goToNextDay}>+</button>
+        </div>
+    </div>;
 }
